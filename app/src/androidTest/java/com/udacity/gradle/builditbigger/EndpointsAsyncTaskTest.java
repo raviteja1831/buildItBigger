fb6d7ae1799955df4e13daffef736189a86901d3
Raviteja <raviteja.tatineni@aa.com>
2019-11-25 13:43:59 -0600

    adding test

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,38 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    Context context;
+
+    @Test
+    public void VerifyTest() {
+        String joke;
+        context = InstrumentationRegistry.getTargetContext();
+        try {
+            joke = new EndpointsAsyncTask().execute(context).get();
+            assertNotNull(joke);
+            assertTrue(joke.length() > 0);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
